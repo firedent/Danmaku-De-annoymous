70ba2fa51e76337bcc0fd0caa8cff78210dfb8df
SuperFashi <admin@superfashi.com>
2016-11-07 22:41:02 +0800

    fixed.

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,10 @@
 var $ = function(term) {
   var t = document.querySelectorAll(term);
-  if(!t){
+  if (!t) {
     return;
-  }else if(t.length == 1){
+  } else if (t.length == 1) {
     return t[0];
-  }else{
+  } else {
     return t;
   }
 }
@@ -15,40 +15,43 @@ var $ = function(term) {
 //   document.addEventListener('DOMContentLoaded', init);
 // }
 
-function bind(sel,eventName,handler){
+function bind(sel, eventName, handler) {
   var el = document.querySelectorAll(sel);
-  if(!el){
+  if (!el) {
     return;
-  }else{
-    for(var i = 0;i < el.length;i++){
-      el[i].addEventListener(eventName,handler);
+  } else {
+    for (var i = 0; i < el.length; i++) {
+      el[i].addEventListener(eventName, handler);
     }
   }
 }
 
-function show(id){
+function show(id) {
   var e = document.getElementById(id);
   e.style.display = '';
   e.scrollIntoView();
 }
-function hide(id){
+
+function hide(id) {
   document.getElementById(id).style.display = 'none';
 }
-function disable(id){
+
+function disable(id) {
   document.getElementById(id).disabled = true;
 }
-function enable(id){
+
+function enable(id) {
   document.getElementById(id).disabled = false;
 }
 
-bind("#frm-vurl","submit",function(e){
-  if(e.preventDefault){
+bind("#frm-vurl", "submit", function(e) {
+  if (e.preventDefault) {
     e.preventDefault();
   }
-  var re = /\/video\/av(\d+)(\/index.html|\/index_(\d+).html)?/; 
+  var re = /\/video\/av(\d+)(\/index.html|\/index_(\d+).html)?/;
   var str = $("#vurl").value;
   var arr = str.match(re);
-  if(!arr || !arr[1]){
+  if (!arr || !arr[1]) {
     $("#vurl").value = "无效的视频地址";
     return false;
   }
@@ -65,10 +68,10 @@ bind("#frm-vurl","submit",function(e){
     hide('loader');
     if (request.status >= 200 && request.status < 400) {
       var data = JSON.parse(request.responseText);
-      if(data.error == 0){
+      if (data.error == 0) {
         window.lastCID = data.cid;
         getComment(data.cid);
-      }else{
+      } else {
         $("#vurl").value = "请求错误";
       }
     } else {
@@ -87,7 +90,7 @@ bind("#frm-vurl","submit",function(e){
 });
 
 
-function getComment(cid){
+function getComment(cid) {
   var request = new XMLHttpRequest();
   request.open('GET', 'https://danmu.fuckbilibili.com/' + cid + '.xml', true);
 
@@ -117,12 +120,12 @@ function getComment(cid){
   request.send();
 }
 
-bind("#frm-searchcm","submit",function(e){
-  if(e.preventDefault){
+bind("#frm-searchcm", "submit", function(e) {
+  if (e.preventDefault) {
     e.preventDefault();
   }
   var keyword = $("#cmsearch").value;
-  if(!keyword){
+  if (!keyword) {
     return false;
   }
 
@@ -130,17 +133,17 @@ bind("#frm-searchcm","submit",function(e){
   show('loader');
 
   var html = '';
-  for(var i = 0 ; i < commentElements.length; i++){
+  for (var i = 0; i < commentElements.length; i++) {
     var n = commentElements[i].childNodes[0];
-    if(n){
+    if (n) {
       var t = n.nodeValue;
-      if(t && t.indexOf(keyword) > -1){
+      if (t && t.indexOf(keyword) > -1) {
         html += cm_tmpl(commentElements[i])
       }
     }
   }
 
-  if(!html || html.length < 1){
+  if (!html || html.length < 1) {
     html = '<tr><td>没有找到任何弹幕，请尝试变更关键词</td></tr>';
   }
 
@@ -153,17 +156,17 @@ bind("#frm-searchcm","submit",function(e){
   return false;
 });
 
-function cm_tmpl(xmlnode){
+function cm_tmpl(xmlnode) {
   var text = xmlnode.childNodes[0].nodeValue.replace(/"/g, "&quot;");
   var shorttext = text;
-  if(text.length > 20){
+  if (text.length > 20) {
     shorttext = text.substr(0, 20);
   }
   var pdata = xmlnode.getAttribute('p').split(',');
   var time = parseInt(pdata[0]);
   var user = pdata[6];
 
-  var minutes = parseInt( time / 60 ) % 60;
+  var minutes = parseInt(time / 60) % 60;
   var minutesa = minutes.toString();
   minuteslength = minutesa.length;
   if (minuteslength <= 1) {
@@ -176,45 +179,47 @@ function cm_tmpl(xmlnode){
     secondsa = '0' + secondsa;
   }
 
-  var str = '<tr><td class="mdl-data-table__cell--non-numeric" title="' + text + '">' + shorttext + '</td><td>' + minutesa + ':' + secondsa+ '</td>\
-                 <td><a id="cmid_' + user + '" href="javascript:;" class="cm-view-btn" onclick="getUser(\''+ user +'\')">爆他菊花</a></td></tr>';
+  var str = '<tr><td class="mdl-data-table__cell--non-numeric" title="' + text + '">' + shorttext + '</td><td>' + minutesa + ':' + secondsa + '</td>\
+                 <td><a id="cmid_' + user + '" style="color: #ff4081" href="javascript:;" class="cm-view-btn" onclick="getUser(\'' + user + '\')">爆他菊花</a></td></tr>';
   return str;
 }
 
-function getUser(user){
+function getUser(user) {
   var el = document.querySelectorAll('#cmid_' + user);
-  for(var i = 0;i < el.length;i++){
+  for (var i = 0; i < el.length; i++) {
+    el[i].style = 'color: #3e3e3e';
     el[i].innerHTML = '正在解析';
   }
-  
+
   var request = new XMLHttpRequest();
-  request.open('GET', apiBase + 'user/hash/' + user, true);
+  request.open('GET', '//api.fuckbilibili.com/uidhash.php?hash=' + user, true);
 
   request.onload = function() {
     if (request.status >= 200 && request.status < 400) {
       var data = JSON.parse(request.responseText);
-      if(data.error == 0){
+      if (data.status) {
         var uid = data.data[0].id;
 
-        window['displayName_' + uid] = function(names1){
-            var name = names1.name;
-            for(var i = 0;i < el.length;i++){
-              el[i].onclick = function(){};
-              el[i].setAttribute('target', '_blank');
-              el[i].href = 'http://space.bilibili.com/' + uid;
-              el[i].innerHTML = name;
+        window['displayName_' + uid] = function(names1) {
+          var name = names1.card.name;
+          for (var i = 0; i < el.length; i++) {
+            el[i].onclick = function() {};
+            el[i].setAttribute('target', '_blank');
+            el[i].style = '';
+            el[i].href = 'http://space.bilibili.com/' + uid;
+            el[i].innerHTML = name;
           }
         }
 
         var script = document.createElement('script');
-        script.setAttribute('type','text/javascript');
-        script.setAttribute('charset','utf-8');
-        script.setAttribute('src','https://api.bilibili.com/userinfo?mid=' + uid +'&type=jsonp&callback=displayName_' + uid);
+        script.setAttribute('type', 'text/javascript');
+        script.setAttribute('charset', 'utf-8');
+        script.setAttribute('src', 'https://account.bilibili.com/api/member/getCardByMid?mid=' + uid + '&type=jsonp&callback=displayName_' + uid);
         document.body.appendChild(script);
 
-      }else{
+      } else {
         alert("用户不存在，这条弹幕是非会员弹幕，也可能是此用户被徐特首删了");
-        for(var i = 0;i < el.length;i++){
+        for (var i = 0; i < el.length; i++) {
           el[i].innerHTML = '用户不存在';
           el[i].onclick = 'javascript:void(0);';
           el[i].href = 'javascript:void(0);';
@@ -222,7 +227,7 @@ function getUser(user){
       }
     } else {
       alert("读取用户失败，服务器错误");
-      for(var i = 0;i < el.length;i++){
+      for (var i = 0; i < el.length; i++) {
         el[i].innerHTML = '解析失败';
       }
     }
@@ -236,7 +241,7 @@ function getUser(user){
   request.send();
 }
 
-function getComment2(cid){
+function getComment2(cid) {
   var request = new XMLHttpRequest();
   request.open('GET', 'https://danmu.fuckbilibili.com/' + cid + '.xml', true);
 
@@ -266,14 +271,14 @@ function getComment2(cid){
   request.send();
 }
 
-bind("#frm-vurl2","submit",function(e){
-  if(e.preventDefault){
+bind("#frm-vurl2", "submit", function(e) {
+  if (e.preventDefault) {
     e.preventDefault();
   }
-  var re = /\/video\/av(\d+)(\/index.html|\/index_(\d+).html)?/; 
+  var re = /\/video\/av(\d+)(\/index.html|\/index_(\d+).html)?/;
   var str = $("#vurl2").value;
   var arr = str.match(re);
-  if(!arr || !arr[1]){
+  if (!arr || !arr[1]) {
     $("#vurl2").value = "无效的视频地址";
     return false;
   }
@@ -290,9 +295,9 @@ bind("#frm-vurl2","submit",function(e){
     hide('loader2');
     if (request.status >= 200 && request.status < 400) {
       var data = JSON.parse(request.responseText);
-      if(data.error == 0){
+      if (data.error == 0) {
         getComment2(data.cid);
-      }else{
+      } else {
         $("#vurl2").value = "请求错误";
       }
     } else {
@@ -310,70 +315,83 @@ bind("#frm-vurl2","submit",function(e){
   return false;
 });
 
-bind("#frm-searchus","submit",function(e){
-  if(e.preventDefault){
+bind("#frm-searchus", "submit", function(e) {
+  if (e.preventDefault) {
     e.preventDefault();
   }
   var user = $("#ussearch").value;
-  if(!user){
+  if (!user) {
     return false;
   }
 
   disable("searchUser");
   show('loader2');
-
-  var request1 = new XMLHttpRequest();
   var html1 = '';
-  request1.open('GET', apiBase + 'userid/' + user, true);
-  request1.onload = function() {
-    if (request1.status >= 200 && request1.status < 400) {
-      var data = JSON.parse(request1.responseText);
-      if(data.error != 0){
-        $("#ussearch").value = "未找到此用户";
-        hide('loader2');
-        enable("searchUser");
-        return false;
-      }
-      var mid = data.uid;
-      var crc = data.crc;
-      for(var i = 0 ; i < commentElements2.length; i++){
-        var n = commentElements2[i].getAttribute('p').split(',')[6];
-        if(n && n.indexOf(crc) > -1){
-          html1 += cm_tmpl2(commentElements2[i])
-        }
-      }
-      if(!html1 || html1.length < 1){
-        html1 = '<tr><td>没有找到任何弹幕，请尝试变更用户</td></tr>';
-      }
-      $('#cmList2').innerHTML = html1;
-    } else {
-      alert("读取用户失败，服务器错误");
+
+  window['finduser'] = function(data) {
+    data = data.cards[user];
+    if (data === null) {
+      $("#ussearch").value = "未找到此用户";
+      hide('loader2');
+      enable("searchUser");
+      return false;
     }
-  };
+    var mid = data.mid;
+    var request = new XMLHttpRequest();
+    request.open('GET', '//api.fuckbilibili.com/uidhash.php?uid=' + mid, true);
 
-  request1.onerror = function() {
-    alert("读取用户失败，网络错误");
-  };
+    request.onload = function() {
+      enable("searchUser");
+      hide('loader2');
+      if (request.status >= 200 && request.status < 400) {
+        show('step2.3');
+        var data2 = JSON.parse(request.responseText);
+        if (data2.status) {
+          var crc = data2.data;
+          for (var i = 0; i < commentElements2.length; i++) {
+            var n = commentElements2[i].getAttribute('p').split(',')[6];
+            if (n && n.indexOf(crc) > -1) {
+              html1 += cm_tmpl2(commentElements2[i]);
+            }
+          }
+          if (!html1 || html1.length < 1) {
+            html1 = '<tr><td>没有找到任何弹幕，请尝试变更用户</td></tr>';
+          }
+          $('#cmList2').innerHTML = html1;
+        } else {
+          $("#ussearch").value = "读取用户失败，请求错误";
+        }
+      } else {
+        $("#ussearch").value = "读取用户失败，服务器错误";
+      }
+    };
 
-  request1.send();
+    request.onerror = function() {
+      alert("读取用户失败，网络错误")
+    };
 
-  enable("searchUser");
-  hide('loader2');
-  show('step2.3');
+    request.send();
+  }
+
+  var script = document.createElement('script');
+  script.setAttribute('type', 'text/javascript');
+  script.setAttribute('charset', 'utf-8');
+  script.setAttribute('src', 'https://account.bilibili.com/api/member/GetInfoByName?uname=' + user + '&type=jsonp&callback=finduser');
+  document.body.appendChild(script);
 
   return false;
 });
 
-function cm_tmpl2(xmlnode){
+function cm_tmpl2(xmlnode) {
   var text = xmlnode.childNodes[0].nodeValue.replace(/"/g, "&quot;");
   var shorttext = text;
-  if(text.length > 20){
+  if (text.length > 20) {
     shorttext = text.substr(0, 20);
   }
   var pdata = xmlnode.getAttribute('p').split(',');
   var time = parseInt(pdata[0]);
 
-  var minutes = parseInt( time / 60 );
+  var minutes = parseInt(time / 60);
   var minutesa = minutes.toString();
   minuteslength = minutesa.length;
   if (minuteslength <= 1) {
